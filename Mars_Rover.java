aea60d792002c0cb46aed4b0d676f2a266230741
ChristianGuerrero <crg41@pitt.com>
2014-05-04 17:17:37 -0400

    Finish main class. Update Rover class
    Finish main class adding checks for each input. While statements used
    to make sure right input is put in. Added javadoc to rover class.

--- Mars_Rover.java
+++ Mars_Rover.java
@@ -1,28 +1,211 @@
-
+/*Author: Christian Guerrero
+*FileName: Mars_Rover
+*Note: if using an IDE (ex. Eclipse) use Scanner if not use System.console
+**/
+import java.util.Scanner;
 public class Mars_Rover {
 	
 
 	
 	public static void main(String[] args) {
+		int counter = 0;
+		Scanner scan = new Scanner (System.in);
 		//used to make sure 
-		boolean inputSatisfied;
-		//grids max values
-		int maxX;
-		int maxY;
+		boolean inputSatisfied = false;
+		//skip next while statements to retry entering info
+		//grids max values and start positions
+		int maxX=10;
+		int maxY=10;
+		int startX = 0;
+		int startY=0;
+		char startO='N';
 		//input string for console 
+		Rover rover1 = new Rover();
+		Rover rover2 = new Rover();
     String input = null;
+    String inputV[];
     
+    
+    //------------------------------------------------------------------------------------------
+    
+    while(!inputSatisfied){
     System.out.println("Enter upper right coordinates:");
-    System.out.println();
-    input = System.console().readLine("Format:<int> <int>");
-    System.out.println(Integer.parseInt(input));
-	//	Rover rover1 = new Rover(1,1,'W');
+    System.out.println("Format:<int> <int>");
+  
+    //input = System.console().readLine();
+    input = scan.nextLine();
     
-		
-		
-	
-	
-	}
-	
-	
+  //ints are seperated by space
+    inputV = input.split(" ");
+    if(inputV.length!=2){
+    	System.out.println("Wrong number of variables");
+    	continue;
+    }
+    try{
+    maxX = Integer.parseInt(inputV[0]);
+    maxY = Integer.parseInt(inputV[1]);
+    }catch(NumberFormatException e){
+    	System.out.println("Numbers are not proper format");
+    	continue;
+    }
+	inputSatisfied = true;
+	}//end of first input while
+    
+    //------------------------------------------------------------------------------------------
+    
+    inputSatisfied = false;
+    //while for rover1 coordinates
+    while(!inputSatisfied){
+        System.out.println("Enter first Rover coordinates:");
+        System.out.println("Format:<int> <int> <N,E,S or W>");
+      
+        //input = System.console().readLine();
+        input = scan.nextLine();
+        
+      //ints are seperated by space
+        inputV = input.split(" ");
+        if(inputV.length!=3){
+        	System.out.println("Wrong number of variables");
+        	continue;
+        }
+        try{
+        startX = Integer.parseInt(inputV[0]);
+        startY = Integer.parseInt(inputV[1]);
+        startO = inputV[2].toUpperCase().charAt(0);
+        
+        if((startO!='N') && (startO!='W')&&(startO!='S')&&(startO!='E')){
+        	System.out.println("Orientation isn't N,W,S or E");
+        	continue;
+        }
+        }catch(NumberFormatException e){
+        	System.out.println("Numbers are not proper format");
+        	continue;
+        }
+        
+    	inputSatisfied = true;
+    	}//end of second input while
+    
+ 
+    //------------------------------------------------------------------------------------------
+  
+    
+    //while for rover1 motion
+    inputSatisfied = false;
+    while(!inputSatisfied){
+    	   rover1.set(startX, startY, maxX, maxY, startO);
+        System.out.println("Enter first Rover motions: Left Right or Move.");
+        System.out.println("Format:<L,R or M>xN");
+      
+        //input = System.console().readLine();
+        input = scan.nextLine();
+        
+      //ints are seperated by space
+        inputV = input.split(" ");
+        if(inputV.length!=1){
+        	System.out.println("Wrong number of variables");
+        	continue;
+        }
+        inputSatisfied = true;
+        for(counter = 0; counter<inputV[0].length();counter++){
+        	
+        	
+        	if(inputV[0].charAt(counter)=='L'){
+        		rover1.rotateLeft();
+        	}
+        	else if(inputV[0].charAt(counter)=='R'){
+        		rover1.rotateRight();
+        	}
+        	else if(inputV[0].charAt(counter)=='M'){
+        		rover1.move();
+        	}
+        	else{
+        		inputSatisfied=false;
+        	}
+        }//end of for
+        if(!inputSatisfied)
+        System.out.println("String contained wrong format");
+        
+    	}//end of third input while
+    
+    //------------------------------------------------------------------------------------------
+    
+    
+  //while for rover2 coordinates
+    inputSatisfied = false;
+    while(!inputSatisfied){
+        System.out.println("Enter second Rover coordinates:");
+        System.out.println("Format:<int> <int> <N,E,S or W>");
+      
+        //input = System.console().readLine();
+        input = scan.nextLine();
+        
+      //ints are seperated by space
+        inputV = input.split(" ");
+        if(inputV.length!=3){
+        	System.out.println("Wrong number of variables");
+        	continue;
+        }
+        try{
+        startX = Integer.parseInt(inputV[0]);
+        startY = Integer.parseInt(inputV[1]);
+        startO = inputV[2].toUpperCase().charAt(0);
+        
+        if((startO!='N') && (startO!='W')&&(startO!='S')&&(startO!='E')){
+        	System.out.println("Orientation isn't N,W,S or E");
+        	continue;
+        }
+        }catch(NumberFormatException e){
+        	System.out.println("Numbers are not proper format");
+        	continue;
+        }
+        
+    	inputSatisfied = true;
+    	}//end of fourth input while
+    
+ 
+    //------------------------------------------------------------------------------------------
+   
+    
+    //while for rover2 motion
+    inputSatisfied = false;
+    while(!inputSatisfied){
+    	   rover2.set(startX, startY, maxX, maxY, startO);
+        System.out.println("Enter second Rover motions: Left Right or Move.");
+        System.out.println("Format:<L,R or M>xN");
+      
+        //input = System.console().readLine();
+        input = scan.nextLine();
+        
+      //ints are seperated by space
+        inputV = input.split(" ");
+        if(inputV.length!=1){
+        	System.out.println("Wrong number of variables");
+        	continue;
+        }
+        inputSatisfied = true;
+        for(counter = 0; counter<inputV[0].length();counter++){
+        	
+        	
+        	if(inputV[0].charAt(counter)=='L'){
+        		rover2.rotateLeft();
+        	}
+        	else if(inputV[0].charAt(counter)=='R'){
+        		rover2.rotateRight();
+        	}
+        	else if(inputV[0].charAt(counter)=='M'){
+        		rover2.move();
+        	}
+        	else{
+        		inputSatisfied=false;
+        	}
+        }//end of for
+        if(!inputSatisfied)
+        System.out.println("String contained wrong format");
+        
+    	}//end of fifth input while
+    
+    System.out.println("Rover1: " + rover1);
+    System.out.println("Rover2: " + rover2);
+    
+}
 }
--- Rover.java
+++ Rover.java
@@ -1,21 +1,57 @@
-
+/*Author: Christian Guerrero
+*FileName: Rover
+*/
 public class Rover {
 	private int x;
 	private int y;
+	private int maxX;
+	private int maxY;
 	private char orientation;
 	
-	//initialization of rover class puts rover in starting position
-	public Rover(int newX,int newY, char newOrientation){
+
+	/**
+	 * Initializes new Rover object with specified parameters
+	 * @param newX 	x starting position
+	 * @param newY 	y starting position
+	 * @param newMaxX 	maximum x value
+	 * @param newMaxY 	maximum y value
+	 * @param newOrientation	starting orientation position 
+	 */
+	public Rover(int newX,int newY,int newMaxX,int newMaxY, char newOrientation){
 		x = newX;
 		y = newY;
+		maxX = newMaxX;
+		maxY = newMaxY;
 		orientation = newOrientation;
 	}
-	//default initialization
+	/**
+	 * Default constructor
+	 */
 	public Rover(){
 		x = 0;
 		y = 0;
 		orientation = 'N';
 	}
+	/**
+	 * Sets values for all of Rover parameters
+	 * 
+	 * @param newX 	x starting position
+	 * @param newY 	y starting position
+	 * @param newMaxX 	maximum x value
+	 * @param newMaxY 	maximum y value
+	 * @param newOrientation	starting orientation position 
+	 */
+	public void set(int newX,int newY,int newMaxX,int newMaxY, char newOrientation){
+		x = newX;
+		y = newY;
+		maxX = newMaxX;
+		maxY = newMaxY;
+		orientation = newOrientation;
+	}
+	/**
+	 *  Rotates rover 90 degrees left
+	 * @return returns new orientation
+	 */
 	public char rotateLeft(){
 		//rover is oriented north
 		if(orientation=='N'){
@@ -35,6 +71,11 @@ public class Rover {
 		
 		return orientation;
 	}
+	
+	/**
+	 * Rotates rover 90 degrees right
+	 * @return returns new orientation
+	 */
 	public char rotateRight(){
 		//rover is oriented north
 				if(orientation=='N'){
@@ -54,28 +95,43 @@ public class Rover {
 				return orientation;
 	}
 	//function assumes that main function checks if move is valid
+	/**
+	 * Moves Rover as specified by orientation
+	 */
 	public void move(){
-		if(orientation=='N'){
+		if(orientation=='N' && y<maxY){
 			y++;
 		}
 		//rover is oriented west
-		else if(orientation=='W'){
+		else if(orientation=='W' && x > 0){
 		  x--;
 		}
 		//rover is oriented south
-		else if(orientation=='S'){
+		else if(orientation=='S' && y > 0){
 			y--;
 		}
-		else if(orientation=='E'){
+		else if(orientation=='E' && x < maxX){
 			x++;
 		}
 	}
 	
-	
+	/**
+	 * Get function for x coordinate
+	 * @return return x coordinate
+	 */
 	public int getX(){
 		return x;
 	}
+	
+	/**
+	 * Get function for y coordinate
+	 * @return
+	 */
 	public int getY(){
 		return y;
+	}
+
+	public String toString(){
+		return (x + " " + y + " " + orientation);
 	}
 }
